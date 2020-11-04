2e1d40aa33666533b3ae7b877853fd3c6427ac86
Ameilia Valdes <valdes.ameilia@gmail.com>
2020-11-04 15:03:49 -0800

    got the images appending to the page

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
 'use strict';
+// imageArray = [];
 
-function Image(image); {
-  this.image_url = image_url;
-  this.title = title;
-  this.description = description;
-  this.keyword = keyword;
-  this.horns = horns;
+function Image(image) {
+  this.image_url = image.image_url;
+  this.title = image.title;
+  this.description = image.description;
+  this.keyword = image.keyword;
+  this.horns = image.horns;
 }
 
-Image.prototype.render = function() {
-  let $imageClone = $('#photo-template');
+Image.prototype.render = function () {
+  let $imageClone = $('#photo-template').clone();
+
+  $('main').append($imageClone);
+
+  $imageClone.find('img').attr('src', this.image_url);
+  $imageClone.find('h2').text(this.name);
+  $imageClone.find('p').text(this.description);
 }
+
+Image.readJson = () => {
+  const ajaxSettings = { method: 'get', dataType: 'json' };
+
+  $.ajax('data/page-1.json', ajaxSettings)
+    .then(data => {
+      data.forEach(item => {
+        let renderedImage = new Image(item);
+        renderedImage.render();
+      })
+    });
+}
+
+$(() => Image.readJson());
